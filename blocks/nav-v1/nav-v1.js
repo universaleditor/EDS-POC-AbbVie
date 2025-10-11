cb09cc897956117dbcb6b725dedcdfb8b2bb75bd
Gopichand0877 <gopichand.reddy.pulagam@ibm.com>
2025-10-11 09:59:35 +0530

    testing models data accessible in js or not

--- blocks/nav-v1/nav-v1.js
+++ blocks/nav-v1/nav-v1.js
@@ -1,24 +1,12 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
-import { moveInstrumentation } from '../../scripts/scripts.js';
-
 export default function decorate(block) {
-  /* change to ul, li */
-  const ul = document.createElement('ul');
-  [...block.children].forEach((row) => {
-    const li = document.createElement('li');
-    moveInstrumentation(row, li);
-    while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
-    });
-    ul.append(li);
-  });
-  ul.querySelectorAll('picture > img').forEach((img) => {
-    const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
-    moveInstrumentation(img, optimizedPic.querySelector('img'));
-    img.closest('picture').replaceWith(optimizedPic);
-  });
-  block.textContent = '';
-  block.append(ul);
+  // TEMP: Just to test if this custom block is rendering!
+  const eyebrow = 'This is my custom Nav V1 Block!';
+
+  // Clear existing content
+  block.innerHTML = '';
+
+  // Insert simple test output
+  const p = document.createElement('p');
+  p.textContent = eyebrow;
+  block.appendChild(p);
 }
